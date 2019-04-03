d5da51775f9c9dbcb7b7ba195a8311a341f9584c
e-landeros <landerosedgard@gmail.com>
2019-04-02 17:42:57 -0700

    created membership model w global choices

--- django_lms/urls.py
+++ django_lms/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.url.static import static 
 from django.contrib import admin
 from django.urls import path
 
--- memberships/models.py
+++ memberships/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+MEMBERSHIP_CHOICES = (
+    ('Enterprise', 'ent'),
+    ('Professional', 'pro'),
+    ('Free', 'free')
+)
+
+class Membership(models.Model):
+    slug = models.SlugField()
+    membership_type = models.CharField(
+        choices=MEMBERSHIP_CHOICES, 
+        default = 'Free',
+        max_length=30)
+    price = models.IntegerField(default=15)
+    stripe_plan_id = models.CharField(max_length=40)
+
+    def __str__(self):
+        return self.membership_type
