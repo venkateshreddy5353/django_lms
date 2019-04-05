003f2a85c61eb0aab030bf20212a41ae565445cb
e-landeros <landerosedgard@gmail.com>
2019-04-04 20:55:43 -0700

    get selected membership convenience method created

--- memberships/views.py
+++ memberships/views.py
@@ -1,14 +1,33 @@
 from django.shortcuts import render
-
+from django.http import HttpResponseRedirect
+from django.contrib import messages
 from django.views.generic import ListView
+from django.urls import reverse
 
-from .models import Membership, UserMembership
+from .models import Membership, UserMembership, Subscription
 
 def get_user_membership(request):
     user_membership_qs = UserMembership.objects.filter(user=request.user)
     if user_membership_qs.exists():
         return user_membership_qs.first()
     return None
+
+def get_user_subscription(request):
+    user_subscription_qs = Subscription.objects.filter(
+        user_membership = get_user_membership(request))
+    if user_subscription_qs.exists():
+        user_subscription = user_subscription_qs.first()
+        return user_subscription
+    return None
+
+#works if selected membership is in the session
+def get_selected_membership(request):
+    membership_type = request.session['selected_membership_type']
+    selected_membership_qs = Membership.objects.filter(membership_type=membership_type)
+    if selected_membership_qs.exists():
+        return selected_membership_qs.first()
+    return None
+    
 
 class MembershipSelectView(ListView):
     model = Membership
@@ -18,3 +37,31 @@ class MembershipSelectView(ListView):
         current_membership = get_user_membership(self.request)
         context['current_membership'] = str(current_membership.membership)
         return context
+
+    def post(self, request, **kwargs):
+        selected_membership = request.POST.get('membership_type') #get the value of the input 
+        user_membership = get_user_membership(request)
+        user_subscription = get_user_subscription(request)
+
+        selected_membership_qs = Membership.objects.filter(
+            membership_type=selected_membership)
+
+        if selected_membership_qs.exists():
+            selected_membership = selected_membership_qs.first()
+        """
+        Validation
+        """
+        if user_membership.membership == selected_membership:
+            if user_subscription != None:
+                messages.info(request, "you already have this membership. Your \
+                    next payment is due {}".format('get this value from stripe'))
+                return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+
+        # assign to the session
+        request.session['selected_membership_type'] = selected_membership.membership_type
+
+        return HttpResponseRedirect(reverse('memberships:payment'))
+
+#provide usser with the stripe payent form and handle the payment
+def PaymentView(request):
+    user_membership = get_user_membership(request)
